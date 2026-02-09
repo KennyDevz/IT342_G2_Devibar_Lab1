40e96160d74196086d9baeb94caaea8bb2a84065
KennyDevz <kenneth.kenn2004@gmail.com>
2026-02-09 23:15:55 +0800

    Added api helper

---
+++ web/src/api.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8080/api";
+
+const api = axios.create({
+    baseURL: API_URL,
+});
+
+// Automatically add the Token to every request if we have one
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export const register = (user) => api.post("/auth/register", user);
+export const login = (credentials) => api.post("/auth/login", credentials);
+export const getCurrentUser = () => api.get("/user/me");
+
+export default api;
