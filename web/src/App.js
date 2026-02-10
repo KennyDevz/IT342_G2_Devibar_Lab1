e0ce7fbe0f27b99290c146d3edd6f7beea28fe3f
KennyDevz <kenneth.kenn2004@gmail.com>
2026-02-10 09:57:42 +0800

    Fixed app.js where the imports of Dashboard and Profile are missing

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
